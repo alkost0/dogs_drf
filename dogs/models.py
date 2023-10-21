2899952b60f9790e0355e7b9af3b0b15f1484ae1
Alexey <alexey.kostenyu@gmail.com>
2023-10-22 01:47:06 +0400

    03 - app users and migrations

--- dogs/models.py
+++ dogs/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Dogs(models.Model):
     name = models.CharField(max_length=100, verbose_name='Кличка')
-    breed = models.ForeignKey("dogs.Breed", verbose_name='Порода собаки', related_name='dogs', on_delete=models.SET_NULL(), null=True)
+    breed = models.ForeignKey("dogs.Breed", verbose_name='Порода собаки', related_name='dogs', on_delete=models.SET_NULL, null=True)
     photo = models.ImageField(upload_to='dog_photo', verbose_name='Фото собаки', null=True, blank=True)
     date_born = models.DateField(verbose_name='Дата рождения', null=True)
-    owner = models.ForeignKey("dogs.User", on_delete=models.CASCADE, )
+#    owner = models.ForeignKey("users.User", on_delete=models.CASCADE, )
 
     class Meta:
         verbose_name = 'Собака'
